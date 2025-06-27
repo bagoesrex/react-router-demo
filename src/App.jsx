3db03e18c296ea6d52d449cc6a91f0e8fee5350e
bagoesrex <bagoesrex@gmail.com>
2025-06-27 22:18:36 +0700

    feat: configuring routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 function App() {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
 
         <h1 class="text-3xl font-bold underline">
-          Hello world!
+          Hello worlds!
         </h1>
       </div>
     </>
