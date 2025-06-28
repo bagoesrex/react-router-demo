f94d73c74395b2906b5125ec271a350375eefb8e
bagoesrex <bagoesrex@gmail.com>
2025-06-28 18:26:31 +0700

    feat: add basic routing for Home, About, and Contact pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router"
 import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
     <>
       <Navbar />
-
+      <div className="flex justify-center items-center w-screen h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
   )
 }
